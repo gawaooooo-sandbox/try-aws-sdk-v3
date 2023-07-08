6e550ee18c7c9f3fd67f782fd4dff1c1690bd42d
gawao <gawao57@gmail.com>
2023-07-08 23:41:48 +0900

    try aws sdk v3

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,32 @@
-export const one = 1;
+import { S3Client, ListBucketsCommand } from "@aws-sdk/client-s3";
+
+const client = new S3Client({});
+
+async function configuration() {
+    console.group('configuration');
+    console.log(await client.config.credentials());
+    console.log(client.config.endpoint);
+    console.groupEnd();
+}
+
+async function listBuckets() {
+    const command = new ListBucketsCommand({});
+    try {
+        const response = await client.send(command);
+        // for(const bucket of response.Buckets || []) {
+        //     console.log(bucket);
+        // }
+
+        console.group('list buckets');
+        console.table(response.Buckets || []);
+        console.log(response.Owner);            
+        console.groupEnd();
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+(async () => {
+    await configuration();
+    await listBuckets();
+})();
